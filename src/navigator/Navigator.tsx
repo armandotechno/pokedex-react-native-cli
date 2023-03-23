25089e2ce3bc08a0e4a73580e13d21b3e1feac66
Armando Capriles <armandotechno12@gmail.com>
2023-03-22 20:26:55 -0400

    Diseño básico del HomeScreen

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -7,9 +7,17 @@ const Stack = createStackNavigator();
 
 export const Navigator = () => {
     return (
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={ HomeScreen } />
-        <Stack.Screen name="Notifications" component={ PokemonScreen } />
+      <Stack.Navigator
+        screenOptions={{
+            headerShown: false,
+            cardStyle: {
+              backgroundColor: 'white'
+            }
+        }}
+      >
+        <Stack.Screen name="HomeScreen" component={ HomeScreen } />
+        <Stack.Screen name="PokemonScreen" component={ PokemonScreen } />
+
       </Stack.Navigator>
     );
 }

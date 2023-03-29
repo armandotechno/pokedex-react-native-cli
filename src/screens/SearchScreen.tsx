664ac8afb599d07d5cde98da16c6f93c88c09c2a
Armando Capriles <armandotechno12@gmail.com>
2023-03-29 15:53:07 -0400

    Pre-cargar la información para buscar por nombre

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,28 @@
-import { Text, View, Platform } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View, Platform } from 'react-native';
+
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { PokemonCard } from '../components/PokemonCard';
 import { SearchInput } from '../components/SearchInput';
+import { usePokemonSearch } from '../hooks/usePokemonSearch';
+import { styles as globalStyles } from '../theme/appTheme';
 
 export const SearchScreen = () => {
 
     const { top } = useSafeAreaInsets();
+    const { isfetching, simplePokemonList } = usePokemonSearch()
+
+    if ( isfetching ) {
+        return (
+            <View style={ styles.activityContainer }>
+                <ActivityIndicator 
+                    size={ 50 }
+                    color="blue"
+                />
+                <Text>Cargando...</Text>
+            </View>
+        )
+    }
 
     return (
         <View style={{ 
@@ -13,6 +31,34 @@ export const SearchScreen = () => {
             marginHorizontal: 20
         }}>
             <SearchInput />
+
+            <FlatList 
+            data={ simplePokemonList }
+            keyExtractor={ (pokemon) => pokemon.id  }
+            showsVerticalScrollIndicator={ false }
+            numColumns={ 2 }
+            renderItem={ ({item}) => (
+              <PokemonCard pokemon={ item }/>
+            )}
+
+            // Header
+            ListHeaderComponent={(
+              <Text style={{ 
+                ...globalStyles.title, 
+                ...globalStyles.globalMargin,
+                padding: 10
+              }}>Pokedex</Text>
+            )}
+          />
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    activityContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
